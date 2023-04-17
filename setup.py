b988a3ce333314d0e171508f2c638478034721b7
alexblnn <alex998@hotmail.fr>
2023-04-17 15:26:05 +0200

    more updates

--- setup.py
+++ setup.py
@@ -4,18 +4,20 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 requirements = ["numpy>=1.15.0", "scipy>=1.0.0",
-                "joblib>=1.0.1", "scikit-learn>=0.22"]
+                "joblib>=1.0.1", "scikit-learn>=0.22",
+                "nilearn","sanssouci","matplotlib",
+                "pandas", "joblib", "tqdm"]
 
 setup(
     name="notip",
-    version="0.1.0",
+    version="0.1.2",
     author="Alexandre Blain",
     author_email="alexandre.blain@inria.fr",
-    description="Nonparametric TDP control for brain imaging",
+    description="Nonparametric True Discovery Proportion control for brain imaging",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/alexblnn/Notip",
-    download_url="https://github.com/alexblnn/Notip/archive/refs/tags/0.1.0.tar.gz",
+    download_url="https://github.com/alexblnn/Notip/releases/download/Notip_OHBM_v2/notip-0.1.2.tar.gz",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
